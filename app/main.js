c7531d48ed776f3de28ea83f81389df41160c7e4
jadecordova <jose.a.de.cordova@gmail.com>
2021-10-18 16:07:57 -0400

    Password enabled

--- app/main.js
+++ app/main.js
@@ -6,18 +6,20 @@ const {
     Menu,
     nativeTheme,
     dialog
-} = require( 'electron' );
+} = require('electron');
 
 const {
     copyFile,
+    exists : fileExists,
     readFile,
     writeFile
-} = require( './io' );
+} = require('./io');
 
 const {
     decryptJSON,
     encryptJSON
-} = require( './encryption' );
+} = require('./encryption');
+const { exists } = require('original-fs');
 
 //----------------------------------------------------------------------------------------------------------------------------------------
 // Since we're gonna be using JSON to store video information instead of a database,
@@ -172,50 +174,46 @@ const menuTemplate = [
                     click: menus // mThumbs
                 }
             ]
-        
+
     }
 ];
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-app.on( 'ready', () =>
-{
+app.on('ready', () => {
     splashWindow = splash();
     encrypted = {
-        stars: readFile( './json/s', true ),
-        videos: readFile( './json/v', true ),
-        tags: readFile( './json/t', true ),
-        settings: readFile( './json/settings', true )
+        stars: readFile('./json/s', true),
+        videos: readFile('./json/v', true),
+        tags: readFile('./json/t', true),
+        settings: readFile('./json/settings', true)
     }
     //nativeTheme.themeSource = 'dark';
 });
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-ipcMain.on( 'password', ( event, args ) =>
-{
+ipcMain.on('password', (event, args) => {
     password = args;
 
     decrypted = {
-        stars: encrypted.stars ? decryptJSON( encrypted.stars, password ) : null,
-        videos: encrypted.videos ? decryptJSON( encrypted.videos, password ) : null,
-        tags: encrypted.tags ? decryptJSON( encrypted.tags, password ) : null,
-        settings: encrypted.settings ? decryptJSON( encrypted.settings, password ) : null
+        stars: encrypted.stars ? decryptJSON(encrypted.stars, password) : null,
+        videos: encrypted.videos ? decryptJSON(encrypted.videos, password) : null,
+        tags: encrypted.tags ? decryptJSON(encrypted.tags, password) : null,
+        settings: encrypted.settings ? decryptJSON(encrypted.settings, password) : null
     }
 
     // Errors in data decryption return an empty string.
-    if ( decrypted.stars === '' )
-    {
-        splashWindow.webContents.send( 'wrong-password' );
+    if (decrypted.stars === '') {
+        splashWindow.webContents.send('wrong-password');
     }
-    else
-    {
+    else {
         // Unset this huge variables.
         encrypted = null;
         mainWindow = main();
     }
-} );
+});
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-ipcMain.on( 'dialog', ( event, {
+ipcMain.on('dialog', (event, {
     type,
     title,
     detail,
@@ -223,121 +221,103 @@ ipcMain.on( 'dialog', ( event, {
     buttonLabel = 'OK',
     message,
     properties = ['openFile'],
-    filters = [{name: 'All Files', extensions: ['*']}]
-} ) =>
-{
-    switch ( type )
-    {
+    filters = [{ name: 'All Files', extensions: ['*'] }]
+}) => {
+    switch (type) {
         case 'confirm':
-            event.returnValue = dialog.showMessageBoxSync( null, {
+            event.returnValue = dialog.showMessageBoxSync(null, {
                 title,
                 type: 'question',
                 detail,
                 buttons: ['OK', 'CANCEL'],
                 message
-            } );
+            });
             break
 
         case 'file':
-            event.returnValue = dialog.showOpenDialogSync( null, {
+            event.returnValue = dialog.showOpenDialogSync(null, {
                 title,
                 buttonLabel,
                 properties,
                 filters
-            } );
+            });
             break
 
         case 'folder':
-            event.returnValue = dialog.showOpenDialogSync( null, {
+            event.returnValue = dialog.showOpenDialogSync(null, {
                 title,
                 buttonLabel,
                 properties: ['openDirectory']
-            } );
+            });
             break
 
         case 'inform':
-            event.returnValue = dialog.showMessageBoxSync( null, {
+            event.returnValue = dialog.showMessageBoxSync(null, {
                 title,
                 type: 'info',
                 message,
                 detail,
                 buttons: ["OK"]
-            } );
+            });
             break
     }
-} );
+});
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-ipcMain.on( 'save-json', ( event, data ) =>
-{
-    const folders = dialog.showOpenDialogSync( null, {
+ipcMain.on('save-json', (event, data) => {
+    const folders = dialog.showOpenDialogSync(null, {
         title: 'Select folder to save JSON files',
         buttonLabel: 'SELECT',
         properties: ['openDirectory']
-    } );
+    });
 
-    if ( folders )
-    {
+    if (folders) {
         const folder = folders[0];
-        const videos = JSON.stringify( data.videos, null, '\t' );
-        const stars = JSON.stringify( data.stars, null, '\t' );
-        const tags = JSON.stringify( data.tags, null, '\t' );
-        writeFile( `${folder}/videos.json`, videos );
-        writeFile( `${folder}/stars.json`, stars );
-        writeFile( `${folder}/tags.json`, tags );
+        const videos = JSON.stringify(data.videos, null, '\t');
+        const stars = JSON.stringify(data.stars, null, '\t');
+        const tags = JSON.stringify(data.tags, null, '\t');
+        writeFile(`${folder}/videos.json`, videos);
+        writeFile(`${folder}/stars.json`, stars);
+        writeFile(`${folder}/tags.json`, tags);
     }
-} );
+});
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-app.on( 'window-all-closed', function ()
-{
-    if ( process.platform !== 'darwin' ) app.quit();
-} );
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-ipcMain.on( 'changed', ( event, file ) =>
-{
+ipcMain.on('changed', (event, file) => {
     changed = true;
-    changedFiles.add( file );
-    Menu.getApplicationMenu().getMenuItemById( 'save' ).enabled = true;
-} );
- 
-//----------------------------------------------------------------------------------------------------------------------------------------
-ipcMain.on( 'receive-data', ( event, {data, quit, password} ) =>
-{
-    save( {data, quit, password} );
-} );
+    changedFiles.add(file);
+    Menu.getApplicationMenu().getMenuItemById('save').enabled = true;
+});
 
 //----------------------------------------------------------------------------------------------------------------------------------------
-ipcMain.on( 'show-context-menu', ( event, result ) =>
-{
+ipcMain.on('receive-data', (event, { data, quit, password }) => {
+    save({ data, quit, password });
+});
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+ipcMain.on('show-context-menu', (event, result) => {
     let template;
 
-    switch ( result.type )
-    {
+    switch (result.type) {
         case 'star':
             template = [
                 {
                     label: 'Edit star',
-                    click: () => {event.sender.send( 'context-menu-command', 'edit-star', result.id )}
+                    click: () => { event.sender.send('context-menu-command', 'edit-star', result.id) }
                 },
                 {
                     label: 'Delete star',
-                    click: () => {event.sender.send( 'context-menu-command', 'delete-star', result.id )}
+                    click: () => { event.sender.send('context-menu-command', 'delete-star', result.id) }
                 },
-                {type: 'separator'},
+                { type: 'separator' },
                 {
                     label: 'Star videos',
-                    click: () => {event.sender.send( 'context-menu-command', 'star-screen', result.id )}
-                },
-                {type: 'separator'},
-                {
-                    label: 'Mark as hard to remember',
-                    click: () => {event.sender.send( 'context-menu-command', 'hard-star', {id: result.id, hard: true} )}
-                },
-                {
-                    label: 'Unmark as hard to remember',
-                    click: () => {event.sender.send( 'context-menu-command', 'hard-star', {id: result.id, hard: false} )}
+                    click: () => { event.sender.send('context-menu-command', 'star-screen', result.id) }
                 }
             ];
             break;
@@ -345,53 +325,51 @@ ipcMain.on( 'show-context-menu', ( event, result ) =>
             break;
     }
 
-    const menu = Menu.buildFromTemplate( template );
+    const menu = Menu.buildFromTemplate(template);
 
-    menu.popup( BrowserWindow.fromWebContents( event.sender ) );
-} );
+    menu.popup(BrowserWindow.fromWebContents(event.sender));
+});
 
 //----------------------------------------------------------------------------------------------------------------------------------------
 /**
  * Creates splash window.
  * @returns {BrowserWindow} Splash window object.
  */
- function splash()
- {
-     let win = new BrowserWindow( {
-         width: 800,
-         height: 519,
-         frame: false,
-         fullscreenable: false,
-         show: false,
-         alwaysOnTop: true,
-         webPreferences: {
-             nodeIntegration: true,
-             contextIsolation: false
-         }
-     } );
- 
-     win.loadFile( 'app/splash.html' );
-     win.on( 'closed', () => {win = null} );
-     win.once( 'ready-to-show', () => {win.show()} );
- 
-     return win;
- }
- 
- //----------------------------------------------------------------------------------------------------------------------------------------
+function splash() {
+    let win = new BrowserWindow({
+        width: 800,
+        height: 519,
+        frame: false,
+        fullscreenable: false,
+        show: false,
+        alwaysOnTop: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile('app/splash.html');
+    win.on('closed', () => { win = null });
+    win.once('ready-to-show', () => { win.show() });
+
+    return win;
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------
 /**
  * Creates main window.
  * @returns {BrowserWindow} Main window object.
  */
-function main()
-{
+function main() {
     // Create main menu.
-    const menu = Menu.buildFromTemplate( menuTemplate );
-    Menu.setApplicationMenu( menu );
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
 
     // Create main window.
-    const {width, height} = screen.getPrimaryDisplay().workAreaSize;
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
-    let win = new BrowserWindow( {
+    let win = new BrowserWindow({
         width: width,
         height: height,
         fullscreenable: true,
@@ -401,42 +379,37 @@ function main()
             enableRemoteModule: true,
             contextIsolation: false
         }
-    } );
+    });
 
-    win.loadFile( 'app/index.html' );
+    win.loadFile('app/index.html');
 
-    win.once( 'ready-to-show', () =>
-    {
-        win.webContents.send( 'data', {
+    win.once('ready-to-show', () => {
+        win.webContents.send('data', {
             password,
             decrypted
-        } );
+        });
 
         decrypted = null;
 
-        win.webContents.send( 'birthdays' );
+        win.webContents.send('birthdays');
         win.webContents.openDevTools();
-        Menu.getApplicationMenu().getMenuItemById( 'save' ).enabled = false;
+        Menu.getApplicationMenu().getMenuItemById('save').enabled = false;
         win.show();
-    } );
+    });
 
-    win.once( 'show', () =>
-    {
-        if ( splashWindow )
-        {
+    win.once('show', () => {
+        if (splashWindow) {
             splashWindow.close();
             splashWindow = null;
         }
-    } );
+    });
 
-    win.once( 'close', ( event ) =>
-    {
-        if ( changed )
-        {
+    win.once('close', (event) => {
+        if (changed) {
             event.preventDefault();
-            getData( true );
+            getData(true);
         }
-    } );
+    });
 
     return win;
 }
@@ -446,27 +419,23 @@ function main()
  * Request updated data from renderer process.
  * @param {boolean} quit - Signals if app should quit. 
  */
- function getData( quit = false )
- {
-     mainWindow.webContents.send( 'send-data', {changedFiles, quit} );
- }
- 
+function getData(quit = false) {
+    mainWindow.webContents.send('send-data', { changedFiles, quit });
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------------
 /**
  * Quits application after checking for unsaved changes.
  */
- function quit()
- {
-     if ( changed )
-     {
-         getData( true );
-     }
-     else
-     {
-         app.quit();
-     }
- }
- 
+function quit() {
+    if (changed) {
+        getData(true);
+    }
+    else {
+        app.quit();
+    }
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------------
 /**
  * Saves changed data.
@@ -475,30 +444,22 @@ function main()
  * @param {boolean} params.quit - Flag indicating if app should quit.
  * @param {string} params.password - Password.
  */
- function save( {data, quit, password} )
- {
-     for ( const [key, value] of Object.entries( data ) )
-     {
-         if ( copyFile( `./json/${key}`, `./json/${key}-backup` ) )
-         {
-             writeFile( `./json/${key}`, encryptJSON( value, password ) );
-         }
-         else
-         {
-             console.error( `Backup of file ${key} failed` );
-         }
-     }
- 
-     changedFiles.clear();
-     changed = false;
-     Menu.getApplicationMenu().getMenuItemById( 'save' ).enabled = false;
- 
-     if ( quit )
-     {
-         app.quit();
-     }
- }
- 
+function save({ data, quit, password }) {
+
+    for (const [key, value] of Object.entries(data)) {
+        if(fileExists(`./json/${key}`)) copyFile(`./json/${key}`, `./json/${key}-backup`);
+        writeFile(`./json/${key}`, encryptJSON(value, password));
+    }
+
+    changedFiles.clear();
+    changed = false;
+    Menu.getApplicationMenu().getMenuItemById('save').enabled = false;
+
+    if (quit) {
+        app.quit();
+    }
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------------
 /**
  * Handles menu selections.
@@ -506,21 +467,18 @@ function main()
  * @param {BrowserWindow} browserWindow - Window.
  * @param {events[]} events - Events.
  */
- function menus( menuItem, browserWindow, events )
- {
-     switch ( menuItem.id )
-     {
-         case 'save':
-             getData();
-             break;
- 
-         case 'exit':
-             quit();
-             break;
- 
-         default:
-             browserWindow.webContents.send( menuItem.id );
-             break;
-     }
- }
- 
+function menus(menuItem, browserWindow, events) {
+    switch (menuItem.id) {
+        case 'save':
+            getData();
+            break;
+
+        case 'exit':
+            quit();
+            break;
+
+        default:
+            browserWindow.webContents.send(menuItem.id);
+            break;
+    }
+}
